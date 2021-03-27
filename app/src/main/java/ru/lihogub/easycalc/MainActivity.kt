7b08842f8072c350c2b921ff19ebec9935a5a8eb
Oleg Lihogub <oligli@yandex.ru>
2021-03-27 22:35:32 +0300

    Added expression evaluation

--- app/src/main/java/ru/lihogub/easycalc/MainActivity.kt
+++ app/src/main/java/ru/lihogub/easycalc/MainActivity.kt
@@ -1,18 +1,14 @@
 package ru.lihogub.easycalc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import java.util.Stack;
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-    private var inputStack  = Stack<String>()
-    private var digitStack  = Stack<String>()
-    private var opStack = Stack<String>()
-    private var tmpStack = Stack<String>()
+    private var calc = Calc()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,70 +16,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateInputExpression(view: View) {
-        findViewById<TextView>(R.id.expressionInput).text = inputStack.toArray().joinToString(separator = "")
+        findViewById<TextView>(R.id.expressionInput).text = calc.getInputExpression()
+        findViewById<TextView>(R.id.expressionOutput).text = calc.evalExpression(findViewById<TextView>(R.id.expressionOutput).text.toString())
     }
 
     fun onEqualButtonClick(view: View) {
-
+        try {
+            var result = calc.eval("2+3+")
+        } catch (e: Exception) {}
     }
 
     fun onDigitButtonClick(view: View) {
-        pushDigit(Integer.parseInt(findViewById<Button>(view.id).text.toString()))
+        calc.pushDigit(Integer.parseInt(findViewById<Button>(view.id).text.toString()))
         updateInputExpression(view)
     }
 
     fun onOperatorButtonClick(view: View) {
-        pushOperator(findViewById<Button>(view.id).text.toString())
+        calc.pushOperator(findViewById<Button>(view.id).text.toString())
         updateInputExpression(view)
     }
 
     fun onDotButtonClick(view: View) {
-        pushDot()
+        calc.pushDot()
         updateInputExpression(view)
     }
 
     fun onDeleteButtonClick(view: View) {
-        try {
-            inputStack.pop()
-        } catch (e: Exception) {}
+        calc.deleteSymbol()
         updateInputExpression(view)
     }
 
-    private fun pushDigit(digit : Int) {
-        inputStack.push(digit.toString())
-    }
-
-    private fun pushDot(){
-        var temporaryStack = Stack<String>()
-        var isError = false
-        var numbersFound = 0
-        while (!inputStack.empty()) {
-            var s = inputStack.pop()
-            temporaryStack.push(s)
-            if (s.equals("+") || s.equals("-") || s.equals("/") || s.equals("*")) break
-            if (s.equals(".")) {
-                isError = true
-                break
-            }
-            numbersFound++
-        }
-        while (!temporaryStack.empty()) inputStack.push(temporaryStack.pop())
-        if (!isError && numbersFound > 0) {
-            inputStack.push(".")
-        }
-    }
-
-    private fun pushOperator(operator : String) {
-        if (inputStack.empty()) return
-        if (inputStack.peek().equals("+")
-            || inputStack.peek().equals("-")
-            || inputStack.peek().equals("*")
-            || inputStack.peek().equals("/")
-        ) {
-            inputStack.pop()
-            inputStack.push(operator)
-            return
-        }
-        inputStack.push(operator);
-    }
 }
